74a0474a707b2fbb3946b1e580af2b82629420c2
Atelier-Mirai <contact@atelier-mirai.net>
2020-10-19 12:10:50 +0900

    routes.rb

---
+++ files/config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  root to: 'users#index'
+  resources :users do
+    member do
+      get :activate
+    end
+  end
+
+  get    'login',  to: 'sessions#new'
+  post   'login',  to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  get    'signup', to: 'users#new'
+
+  resources :password_resets, only: [:create, :edit, :update]
+
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+end
