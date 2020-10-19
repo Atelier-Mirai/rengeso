ffef7f05996fe45d6e60a4581af0cbe342e55fd5
Atelier-Mirai <contact@atelier-mirai.net>
2020-10-19 10:52:26 +0900

    fix

--- files/app/helpers/application_helper.rb
+++ files/app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title(page_title = nil)
-    base_title = "さくら商会"
+    base_title = "株式会社蓮華草"
     base_title << "【開発】" if Rails.env.development?
     page_title.nil? ? base_title : "#{page_title} | #{base_title}"
   end
